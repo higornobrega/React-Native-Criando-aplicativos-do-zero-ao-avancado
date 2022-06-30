18eb93052862f4a19dfeb7f01c972ac075c96b93
Higor Nóbrega <higorst.nobrega@gmail.com>
2022-06-30 01:40:59 -0300

    13. Entendendo as States

--- meuAppExpo/App.js
+++ meuAppExpo/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import {
-  Image,
+  Button,
   Text,
   View,
 } from 'react-native';
@@ -17,43 +17,74 @@ import {
   )
 }
 */
-
-//Em forma de Componentização
-class App extends Component{
-  render(){
-    let nome = 'Steve Jobs';
-    return(
-      <View>
-        <Text>
-          Olá mundo!
-        </Text>
-         <Text>
-          Olá mundo!
-        </Text>
-        <Text style={{ color:'#FF0000', fontSize:25, margin:15}}> 
-          Meu Primeiro App
-        </Text>
+ 
+//Em forma de Componentização com props
+// class App extends Component{
+//   render(){
+//     let nome = 'Steve Jobs';
+//     return(
+//       <View>
+//         <Text>
+//           Olá mundo!
+//         </Text>
+//          <Text>
+//           Olá mundo!
+//         </Text>
+//         <Text style={{ color:'#FF0000', fontSize:25, margin:15}}> 
+//           Meu Primeiro App
+//         </Text>
         
-        <Jobs largura={500} altura={200} fulano={"Steve Jobs"} //Enviando props
-        />
+//         <Jobs largura={500} altura={200} fulano={"Steve Jobs"} //Enviando props
+//         />
 
-      </View>
-    );
+//       </View>
+//     );
+//   }
+// }
+
+
+// class Jobs extends Component{
+//   render(){
+//     let url_img = 'https://sujeitoprogramador.com/steve.png';
+//     return(
+//       <View>
+//         <Image
+//             source={{ uri: url_img }}
+//             style={{ width:this.props.largura, height:this.props.altura}} //Recebendo Props
+//         />
+//         <Text style={{ fontSize:30}}>
+//         {this.props.fulano}
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
+
+
+//Em forma de Componentização com states
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      nome: ''
+    }
+    this.entrar = this.entrar.bind(this) //Deixa a função visível para a classe
   }
-}
 
+  entrar(nome){
+    this.setState({
+      nome:nome
+    })
+  }
 
-class Jobs extends Component{
   render(){
-    let url_img = 'https://sujeitoprogramador.com/steve.png';
     return(
-      <View>
-        <Image
-            source={{ uri: url_img }}
-            style={{ width:this.props.largura, height:this.props.altura}} //Recebendo Props
-        />
-        <Text style={{ fontSize:30}}>
-        {this.props.fulano}
+      <View style={{marginTop: 20}}>
+        <Button title='Entrar' onPress={() => this.entrar('Higor')}/>
+
+        <Text style={{fontSize:23, color:'red', textAlign:'center'}}>
+          {this.state.nome}
         </Text>
       </View>
     );
