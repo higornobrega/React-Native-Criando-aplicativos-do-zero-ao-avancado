dc494f64c7a1e520489b6b7415462d4436e1d736
Higor Nóbrega <higorst.nobrega@gmail.com>
2022-07-01 13:10:33 -0300

    19. Trabalhando com botões

--- meuAppExpo/App.js
+++ meuAppExpo/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import {
+  Button,
   StyleSheet,
   Text,
   TextInput,
@@ -186,24 +187,80 @@ import {
 // }
 
 //Input
+// class App extends Component{
+
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       nome : ''
+//     }
+//     this.pegaNome = this.pegaNome.bind(this)
+//   }
+
+//   pegaNome(texto){
+//     if(texto.length > 0){
+
+//       this.setState({nome:'Bem Vindo ' + texto})
+//     } else {
+//       this.setState({nome:''})
+
+//     }
+//   }
+
+//   render(){
+
+//     return(
+//       <View style={styles.container}>
+//         <TextInput 
+//         style={styles.input} 
+//         placeholder="Digite seu nome"
+//         underlineColorAndroid="transparente"
+//         onChangeText={this.pegaNome}
+//         />
+//         <Text style={styles.texto}>{this.state.nome}</Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container:{
+//     flex: 1,
+//   },
+//   input:{
+//     height: 45,
+//     borderWidth:1,
+//     borderColor:'#222',
+//     margin: 10,
+//     marginTop:40,
+//     fontSize:20,
+//     padding: 10,
+//   },
+//   texto:{
+//     textAlign:'center',
+//     fontSize:25,
+//   }
+// })
+
+
+//Input + button
 class App extends Component{
 
   constructor(props){
     super(props);
     this.state = {
-      nome : ''
+      nome : '',
+      input:''
     }
-    this.pegaNome = this.pegaNome.bind(this)
+    this.entrar = this.entrar.bind(this)
   }
 
-  pegaNome(texto){
-    if(texto.length > 0){
-
-      this.setState({nome:'Bem Vindo ' + texto})
-    } else {
-      this.setState({nome:''})
-
+  entrar(){
+    if(this.state.input === ''){
+      alert("Digite Seu nome");
+      return
     }
+    this.setState({nome: 'Bem Vindo ' + this.state.input})
   }
 
   render(){
@@ -214,7 +271,11 @@ class App extends Component{
         style={styles.input} 
         placeholder="Digite seu nome"
         underlineColorAndroid="transparente"
-        onChangeText={this.pegaNome}
+        onChangeText={(texto)=> this.setState({input: texto})}
+        />
+        <Button
+          title='Entrar'
+          onPress={this.entrar}
         />
         <Text style={styles.texto}>{this.state.nome}</Text>
       </View>
@@ -240,5 +301,6 @@ const styles = StyleSheet.create({
     fontSize:25,
   }
 })
+
 
 export default App;
